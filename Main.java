803cac5b08aab260eba96adcd7e6835cbae9743f
Otzar Jaffe <34491649+yellowstonepark@users.noreply.github.com>
2023-09-11 08:36:45 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class MyFirstJavaProgram {
 
    public static void main(String []args) {
       System.out.println("Hello World"); // prints Hello World
+      System.out.println("Hi")
    }
-   
+}
