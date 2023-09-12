dc401c8064539d72a785fff89622ba0b1a49cd0f
Otzar Jaffe <otzarjaffe@gmail.com>
2023-09-12 08:39:36 -0400

    adding file

---
+++ classwork/00_hello_world/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int x = 5;
+        for (int i = 0; i < 5; i++) {
+            System.out.println(x * i);
+        }
+    }
+}
